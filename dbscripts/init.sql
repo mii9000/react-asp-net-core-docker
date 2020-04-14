e75f4d251ae01664ed42de7913f7abd5b09bca03
ibrahim-islam <ibrahim.islam@live.com>
2020-04-15 02:02:09 +0700

    adds me in the data

--- dbscripts/init.sql
+++ dbscripts/init.sql
@@ -31,6 +31,7 @@ INSERT INTO users(id, name, email) VALUES(8, 'Dr. Watson', 'example@org.com');
 INSERT INTO users(id, name, email) VALUES(9, 'Bill Gates', 'example@org.com');
 INSERT INTO users(id, name, email) VALUES(10, 'Steven Jobs', 'example@org.com');
 INSERT INTO users(id, name, email) VALUES(11, 'Elon Musk', 'example@org.com');
+INSERT INTO users(id, name, email) VALUES(12, 'Mohammed Ibrahim Islam', 'dev.ibrahim.islam@gmail.com');
 
 INSERT INTO groups(id, name, description) VALUES(1, 'Super Heros', 'A place for super heros to get together and discuss on how to save the world');
 INSERT INTO groups(id, name, description) VALUES(2, 'Doe Family', 'A group for the Doe family to catch up at the end of the day');
@@ -38,26 +39,26 @@ INSERT INTO groups(id, name, description) VALUES(3, 'Detectives', 'Come join if 
 INSERT INTO groups(id, name, description) VALUES(4, 'Billion Dollar Club', 'Only for the high and mighty!');
 
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(1, 5, false);
-INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(1, 6, true);
+INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(1, 6, false);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(1, 7, false);
+INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(1, 12, true);
 
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(2, 1, true);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(2, 2, false);
 
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(3, 3, false);
-INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(3, 4, true);
+INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(3, 4, false);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(3, 8, false);
+INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(3, 12, true);
 
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(4, 9, true);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(4, 10, false);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(4, 11, false);
 INSERT INTO user_groups(group_id, user_id, is_admin) VALUES(4, 5, false);
 
-
 SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users));
 
 SELECT SETVAL('groups_id_seq', (SELECT MAX(id) FROM groups));
-
 
 SELECT currval('users_id_seq');
 
